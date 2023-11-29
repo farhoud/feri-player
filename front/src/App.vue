524f3b3f3ec09bdc60ae86fc6c4df96462b50be5
farhoud.m7 <fymc5jspqhaqxwnt3z63myz6tv2kqls75xvtzgpbkh2e2unpl6vq>
2023-11-29 14:54:33 +0330

    add video source component

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <v-app>
     <v-main>
+      <video-source @open="(link)=>{url=link}">
+
+      </video-source>
       <HelloWorld
+        v-if="url"
         subtitle-source="/episode4.vtt"
         :video-source="url"
       />
@@ -11,12 +15,8 @@
 
 <script setup lang="ts">
 import HelloWorld from "@/components/VideoPlayer.vue";
-import { computed } from "vue";
+import videoSource from "@/components/VideoSource.vue"
+import { ref } from "vue";
 
-const url = computed(()=> {
-  // min and max included
-  const min = 1, max = 8
-  const eps = Math.floor(Math.random() * (max - min + 1) + min);
-  return `https://dls5.top-film2serial.click/DonyayeSerial/series/Biohackers/Soft.Sub/S01/720p.Web-DL/Biohackers.S01E0${eps}.720p.WEB-DL.SoftSub.DonyayeSerial.mkv`
-})
+const url = ref()
 </script>
---
+++ front/src/components/VideoSource.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-sheet width="300" class="mx-auto">
+    <v-form @submit.prevent="open(fileLink)">
+      <v-text-field
+        v-model="fileLink"
+        :rules="rules"
+        label="File Link"
+      ></v-text-field>
+      <v-btn type="submit" block class="mt-2">Open</v-btn>
+    </v-form>
+  </v-sheet>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const emit = defineEmits(["open"])
+const fileLink = ref();
+const rules = [
+  (value) => {
+    if (value) return true;
+
+    return "You must enter a first name.";
+  },
+];
+const open = (link)=>{
+  emit("open", link)
+}
+</script>
